768540f1639ed0b636b0ad0d0ada8228f38f1be2
OpTi9 <55547016+OpTi9@users.noreply.github.com>
2023-07-23 18:29:36 +0200

    working on auth page

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -3,6 +3,7 @@
 import {useCallback, useState} from "react";
 import {FieldValues, SubmitHandler, useForm} from "react-hook-form";
 import Input from "@/app/components/inputs/Input";
+import Button from "@/app/components/Button";
 
 type Variant = 'login' | 'register';
 
@@ -54,13 +55,50 @@ const AuthForm = () => {
                     className='space-y-6'
                     onSubmit={handleSubmit(onSubmit)}
                 >
+                    {variant === 'register' && (
+                        <Input
+                            errors={errors}
+                            label="Name"
+                            register={register}
+                            id='name'
+                        />
+                    )}
                     <Input
                         errors={errors}
                         label="Email"
                         register={register}
+                        type="email"
                         id='email'
                     />
+                    <Input
+                        errors={errors}
+                        type="password"
+                        label="Password"
+                        register={register}
+                        id='password'
+                    />
+                    <div>
+                        <Button
+                            disabled={isLoading}
+                            fullWidth
+                            type='submit'
+                        >
+                            {variant === 'login' ? 'Sign in' : 'Register'}
+                        </Button>
+                    </div>
                 </form>
+                <div className='mt-6'>
+                    <div className='relative'>
+                        <div className='absolute inset-0 flex items-center'>
+                            <div className="w-full border-t border-gray-300"/>
+                        </div>
+                        <div className="relative flex justify-center text-sm leading-5 px-2">
+                            <span className="bg-white px-2 text-gray-500">
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
